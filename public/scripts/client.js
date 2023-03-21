7e26e6c7ec228043fdef705f95866030fd829085
TheLoneliestTime <john.a.roxas@gmail.com>
2023-03-20 18:08:46 -0700

    re-added the feature to empty the textbox on successful tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -80,14 +80,10 @@ that matches our original html structure for a tweet
     } else if (input.length > 140) {
       alert("Tweet is too long! Consider cutting down!");
     } else {
-      jQuery
-        .post("/tweets", $(this).serialize(), () => {
-          console.log("successful message sent to server");
-        })
-        .done(() => {
-          console.log("loading tweets...");
-          loadTweets();
-        });
+      jQuery.post("/tweets", $(this).serialize()).done(() => {
+        loadTweets();
+        $("#tweet-text").val("");
+      });
     }
 
     event.preventDefault();
