f7c128cab64c864a277ad396a4a5f91ceb28c6cd
TheLoneliestTime <john.a.roxas@gmail.com>
2023-03-20 18:07:00 -0700

    it now runs the POST method correctly as soon as the message is sent to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -67,6 +67,30 @@ that matches our original html structure for a tweet
       renderTweets(input);
     });
   };
-  loadTweets();
+
+  // Form submission functionality.
+
+  $("#target").submit(function (event) {
+    // Writing a shortcut to the input form so we don't have a lot of unnecessary code.
+    let input = $(this).serialize().slice(5);
+    if (!input) {
+      alert("Write something in the textbox! Cannot tweet an empty message!");
+    } else if (input === "%20") {
+      alert("Write something in the textbox! Cannot tweet an empty message!");
+    } else if (input.length > 140) {
+      alert("Tweet is too long! Consider cutting down!");
+    } else {
+      jQuery
+        .post("/tweets", $(this).serialize(), () => {
+          console.log("successful message sent to server");
+        })
+        .done(() => {
+          console.log("loading tweets...");
+          loadTweets();
+        });
+    }
+
+    event.preventDefault();
+  });
   // renderTweets(data);
 });
--- public/scripts/formSubmission.js
+++ public/scripts/formSubmission.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
-  $("#target").submit(function (event) {
-    // Writing a shortcut to the input form so we don't have a lot of unnecessary code.
-    let input = $(this).serialize().slice(5);
-    if (!input) {
-      alert("Write something in the textbox! Cannot tweet an empty message!");
-    } else if (input === "%20") {
-      alert("Write something in the textbox! Cannot tweet an empty message!");
-    } else if (input.length > 140) {
-      alert("Tweet is too long! Consider cutting down!");
-    } else {
-      jQuery.post("/tweets", $(this).serialize());
-      $("#tweet-text").val("");
-    }
-
-    event.preventDefault();
-  });
+  // $("#target").submit(function (event) {
+  //   // Writing a shortcut to the input form so we don't have a lot of unnecessary code.
+  //   let input = $(this).serialize().slice(5);
+  //   if (!input) {
+  //     alert("Write something in the textbox! Cannot tweet an empty message!");
+  //   } else if (input === "%20") {
+  //     alert("Write something in the textbox! Cannot tweet an empty message!");
+  //   } else if (input.length > 140) {
+  //     alert("Tweet is too long! Consider cutting down!");
+  //   } else {
+  //     jQuery.post("/tweets", $(this).serialize());
+  //     $("#tweet-text").val("");
+  //   }
+  //   event.preventDefault();
+  // });
 });
